e6432aedff423574f3da8d1f9bbcb5e40a97c84e
Christian Trabold <info@christian-trabold.de>
2014-05-16 15:51:46 +0700

    spec: Allow running in `exec` mode on Travis-CI
    If the ENV variable `TRAVIS=true` then we execute the tests locally.
    
    Otherwise we run them via SSH on our local VM.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,52 +1,46 @@
-require "highline/import"
 require "serverspec"
-require "pathname"
-require "net/ssh"
-require "pp"
 
-include SpecInfra::Helper::Ssh
 include SpecInfra::Helper::DetectOS
 include Serverspec::Helper::Properties
 
-RSpec.configure do |c|
-  if ENV["ASK_SUDO_PASSWORD"]
-    c.sudo_password = ask("Enter sudo password: ") { |q| q.echo = false }
-  else
-    c.sudo_password = ENV["SUDO_PASSWORD"]
-  end
+# Run tests locally WITHOUT SSH on travis-ci
+if ENV["TRAVIS"]
+  include SpecInfra::Helper::Exec
+else
+  # Run tests via SSH when testing locally on a VM
+  RSpec.configure do |c|
+    require "net/ssh"
+    require "highline/import"
+    include SpecInfra::Helper::Ssh
 
-  target_vm  = ENV["VAGRANT_VM_NAME"]
+    c.before :suite do
+      options = {}
+      target_vm  = ENV["VAGRANT_VM_NAME"]
 
-  c.ssh.close if c.ssh
+      c.ssh.close if c.ssh
 
-  say "running 'vagrant up #{target_vm}'"
-  say "this can take quite a while, if the box is not yet up!"
-  vagrant_up = `vagrant up #{target_vm}`
-  say "running 'vagrant ss-config #{target_vm}'"
-  config = `vagrant ssh-config #{target_vm}`
+      say "running 'vagrant up #{target_vm}'"
+      say "this can take quite a while, if the box is not yet up!"
+      vagrant_up = `vagrant up #{target_vm}`
+      say "running 'vagrant ss-config #{target_vm}'"
+      config = `vagrant ssh-config #{target_vm}`
 
-  if config != ""
-    host = user = keys = port = ""
-
-    config.each_line do |line|
-      if match = /HostName (.*)/.match(line)
-        host = match[1]
-      elsif  match = /User (.*)/.match(line)
-        user = match[1]
-      elsif match = /IdentityFile (.*)/.match(line)
-        keys =  [match[1].gsub(/"/,'')]
-      elsif match = /Port (.*)/.match(line)
-        port = match[1]
+      if config != ""
+        config.each_line do |line|
+          if match = /HostName (.*)/.match(line)
+            c.host = match[1]
+          elsif  match = /User (.*)/.match(line)
+            options[:user] = match[1]
+          elsif match = /IdentityFile (.*)/.match(line)
+            options[:keys] =  [match[1].gsub(/"/,"")]
+          elsif match = /Port (.*)/.match(line)
+            options[:port] = match[1]
+          end
+        end
+        c.ssh = Net::SSH.start(c.host, options[:user], options)
+      else
+        raise("Could not find ssh configuration for #{target_vm}")
       end
     end
-
-    options = Net::SSH::Config.for(host)
-    options[:port] = port
-    options[:keys] = keys
-
-    c.ssh   = Net::SSH.start(host, user, options)
-  else
-    raise("Could not find ssh configuration for #{target_vm}")
   end
-
 end
